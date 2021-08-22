e58af7d8a83ec9887b5449eb75f42d52b83fa0ac
Stackbit Code Editor <projects@stackbit.com>
2021-08-22 23:35:10 +0000

    page.js: changed

--- src/layouts/page.js
+++ src/layouts/page.js
@@ -22,6 +22,5 @@ export default class Page extends React.Component {
                 </article>
             </Layout>
 
-<iframe src="https://app.acuityscheduling.com/schedule.php?owner=23252019&calendarID=5662289" title="Schedule Appointment" width="100%" height="800" frameBorder="0"></iframe><script src="https://embed.acuityscheduling.com/js/embed.js" type="text/javascript"></script>        );
     }
 }
